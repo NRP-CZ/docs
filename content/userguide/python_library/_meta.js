021328198d7675b1348154fc8e6f7f510093ba95
Mirek Simek <3385642+mesemus@users.noreply.github.com>
2026-01-19 22:06:04 +0100

    cmdline api

---
+++ content/userguide/python_library/_meta.js
@@ -0,0 +1,26 @@
+export default {
+    "sync_library": {
+        "title": "Sync Client"
+    },
+    "sync_library_records": {
+        "title": "Records API (Sync)"
+    },
+    "sync_library_files": {
+        "title": "Files API (Sync)"
+    },
+    "sync_library_requests": {
+        "title": "Requests API (Sync)"
+    },
+    "async_library": {
+        "title": "Async Client"
+    },
+    "async_library_records": {
+        "title": "Records API (Async)"
+    },
+    "async_library_files": {
+        "title": "Files API (Async)"
+    },
+    "async_library_requests": {
+        "title": "Requests API (Async)"
+    }
+}
