162d89a53880c7e98b2abaf10b1cf9c5e9eaa9c8
Miroslav Simek <3385642+mesemus@users.noreply.github.com>
2026-03-20 07:50:10 +0100

    docs

--- content/userguide/_meta.js
+++ content/userguide/_meta.js
@@ -1,8 +1,11 @@
 export default {
-  "index": {
-    "title": "User Guide",
+  index: {
+    title: "User Guide",
   },
-  "commandline": {
-    "title": "Commandline tools"
-  }
-}
+  commandline: {
+    title: "Commandline tools",
+  },
+  api_docs: {
+    title: "REST API documentation",
+  },
+};
